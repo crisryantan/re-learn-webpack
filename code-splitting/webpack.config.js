37520f578aa2500fba08bcf35aca6be016892580
Cris Ryan Tan <crisryantan@gmail.com>
2020-04-14 00:19:14 +1000

    preload

--- code-splitting/webpack.config.js
+++ code-splitting/webpack.config.js
@@ -8,7 +8,7 @@ const presetConfig = require("./build-utils/loadPresets");
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
   return webpackMerge(
     {
-      mode,
+      mode: "none",
       module: {
         rules: [
           {
